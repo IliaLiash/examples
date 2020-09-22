adcf0c82094275dcf105f7e0212a52be4c16dfb1
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2020-09-22 15:07:33 +0300

    Add files via upload

---
+++ python_math/numpy/numpy_first_vectors.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+s = list(map(float, input().split(',')))
+
+V1 = np.array(s))
+V2 = np.array(s[-2])
+V3 = np.array(s[::-1])
+V4 = np.array(s[0::3])
+V5 = np.array(range(len(s)))
