7637fe1fc2133f5eb5c625e5bfd3a4afa8937310
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2020-09-26 12:09:37 +0300

    Update and rename xlsx_region_vasya.py to python_xlsx/xlsx_region_vasya.py

--- python_xlsx/xlsx_region_vasya.py
+++ python_xlsx/xlsx_region_vasya.py
@@ -27,10 +27,16 @@ for k, v in medians.items():
 average = {}
 for i in range(1,8):
         a = sheet.col_values(i)[1:]
-        result = [int(item) for item in a]
+        result = [int(item) for item in
         average[sheet.col_values(i)[0]] = mean(result)
         
 for k, v in average.items():
         if v == max(average.values()):
                 print(k, v)
                 break
+'''                
+Вася планирует карьеру и переезд. Для это составил таблицу, в которой для каждого региона записал зарплаты для разных интересные ему профессий.
+Таблица доступна по ссылке https://stepik.org/media/attachments/lesson/245267/salaries.xlsx.
+Выведите название региона с самой высокой медианной зарплатой (медианой называется элемент, стоящий в середине массива после его упорядочивания)
+и, через пробел, название профессии с самой высокой средней зарплатой по всем регионам.
+'''
