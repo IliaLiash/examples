a004c2fe6c62d60fd85abc979e5fc3b686970e2a
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2020-10-02 19:33:59 +0300

    Update dict_capitals.py

--- dict_capitals.py
+++ dict_capitals.py
@@ -2,11 +2,13 @@
 
 n = int(input())
 m = {}
+
 for i in range(n):
     country, *cities = input().split()
     
     for city in cities:
         m[city] = country
+
 m = int(input())
 
 for i in range(k):
