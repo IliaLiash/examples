ce6ca0ceba372d417b9109d4a6c7662bf2d76f16
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2020-10-05 00:31:14 +0300

    Add files via upload

---
+++ class_neuron.py
@@ -0,0 +1,17 @@
+class Neuron:
+
+    def __init__(self, w, f = lambda x: x):
+        self.w = w
+        self.f = f
+        self.x = None
+        
+    def forward(self, x):
+        self.x = x
+        total = 0
+        for i in range(len(w)):
+            total += x[i] * w[i]
+            
+        return self.f(total) 
+
+    def backlog(self):
+        return self.x
