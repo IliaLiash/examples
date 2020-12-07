e95acec80badbb9380090758272369ced9729cd4
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2020-12-07 11:09:10 +0200

    Add files via upload

---
+++ def_map_rooms_dict.py
@@ -0,0 +1,12 @@
+rooms = [
+    {"name": "кухня", "length": 6, "width": 4},
+    {"name": "комната 1", "length": 5.5, "width": 4.5},
+    {"name": "комната 2", "length": 5, "width": 4},
+    {"name": "комната 3", "length": 7, "width": 6.3},
+]
+
+def square(d):
+    d['square'] = d['length'] * d['width']
+    return d
+
+rooms = map(square, rooms)
