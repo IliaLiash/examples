b992914b0d58889d9cdff37564d9c4a9c11c55ca
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2020-12-16 11:51:20 +0200

    Add files via upload

---
+++ checkio/highest_building.py
@@ -0,0 +1,43 @@
+import numpy as np
+def highest_building(buildings):
+    buildings = np.array(buildings).sum(axis=0).tolist()    
+    return [buildings.index(max(buildings)) + 1, max(buildings)]
+
+if __name__ == '__main__':
+    print("Example:")
+    print(highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]) == [3, 4], "Common"
+    assert highest_building([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 1]
+    ]) == [4, 1], "Cabin in the wood"
+    assert highest_building([
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1]
+    ]) == [1, 5], "Triangle"
+    assert highest_building([
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1]
+    ]) == [4, 6], "Pyramid"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
