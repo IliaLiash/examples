ad015173c490203e07f7f3e66fa564ad79688721
Ilia LIash <70135336+IliaLiash@users.noreply.github.com>
2021-01-02 23:35:26 +0200

    Update mymap.py

--- mymap.py
+++ mymap.py
@@ -1,9 +1,6 @@
 def mymap(func, *args):
-    for element in args:
-        a = list(map(func, *args))
-    return a
+    return list(map(func, *args))
 
-'''
 numbers1 = [1, 2, 3]
 numbers2 = [4, 5, 6]
 
@@ -12,4 +9,3 @@ print(mymap(lambda x, y: x + y, numbers1, numbers2))
 print(mymap(str.upper, ['a', 'b', 'c', 'd', 'e']))
 
 print(mymap(lambda x: x**2, [1, 2, 3, 4, 5]))
-'''
